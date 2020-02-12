58f2059692d1266216a045e88e430cdcabcb5371
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-02-12 03:18:51 +0300

    create lesson13

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let headerInput = document.querySelector('.header-input'),
+    plusButton = document.querySelector('.header-button'),
+    toDoItem = document.querySelectorAll('.todo-item'),
+    toDoRemove = document.querySelectorAll('.todo-remove'),
+    toDoComplete = document.querySelectorAll('.todo-complete'),
+    toDo = document.querySelector('.todo'),
+    toDoCompleted = document.querySelector('.todo-completed'),
+    toDoButtons = document.querySelectorAll('.todo-buttons');
+
+let clone = toDoItem[0].cloneNode(true);
+toDo.removeChild(toDoItem[0]);
+toDoCompleted.removeChild(toDoItem[1]);
+console.log(toDoItem.length);
+let info;
+function getInfo () {
+  event.preventDefault();
+  if(headerInput.value !== ''){
+  info = headerInput.value;
+  let clons = info;
+  localStorage.setItem(clons, info);
+  // console.log(localStorage.lel)
+  addBusiness();
+  headerInput.value = '';
+
+ 
+  }
+}
+function addBusiness () {
+  let clone2 = clone.cloneNode(true);
+  clone2.innerHTML = info;
+  let buttonsClone = toDoButtons[0].cloneNode(true);
+  
+  clone2.appendChild(buttonsClone);
+  toDo.appendChild(clone2);
+  buttonsClone.children[0].addEventListener('click', function () {
+  if(clone2.parentNode === toDoCompleted) {
+    toDoCompleted.removeChild(clone2);
+  } else {
+    toDo.removeChild(clone2);
+  }
+  toDoRemove = document.querySelectorAll('.todo-remove');
+  
+  
+  })
+  buttonsClone.children[1].addEventListener('click', function () {
+    toDoRemove = document.querySelectorAll('.todo-remove');
+    if(clone2.parentNode === toDo){
+      toDoCompleted.appendChild(clone2);
+    } else {
+      toDo.appendChild(clone2);
+    }
+  
+    })
+  
+}
+
+plusButton.addEventListener('click', getInfo)
